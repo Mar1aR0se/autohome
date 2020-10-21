9f6d68a9c857a44b8996549150bb92a3eb66380e
Hatsunoharu <57436539+Mar1aR0se@users.noreply.github.com>
2020-10-21 15:09:50 +0700

    First

---
+++ src/main/java/de/commsult/autohome/App.java
@@ -0,0 +1,13 @@
+package de.commsult.autohome;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
